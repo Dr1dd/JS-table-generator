dc3dd4c168241a7e24b9168ac03c32228589f88c
Laurynas Leišis <lauris.leisis@gmail.com>
2019-12-03 20:29:43 +0200

    only media left

--- tablegen.js
+++ tablegen.js
@@ -35,7 +35,6 @@ $(function() {
 		} 
 
 
-
 	$( ".confirmbtn" ).click(function(){
 
 		sessionStorage.setItem("rowCount", document.getElementById('rowCount').value);
@@ -49,6 +48,8 @@ $(function() {
 		sessionStorage.setItem("columnTitles", JSON.stringify(columnTitles));
 		sessionStorage.setItem("height", document.getElementById('height').innerHTML);
 		sessionStorage.setItem("width", document.getElementById('width').innerHTML);
+		sessionStorage.setItem("borderStyle", $('input[name=border]:checked').val());
+		console.log($('input[name=border]:checked').val());
 		var width = document.getElementById('width').innerHTML;
 		var columns = document.getElementById('columnCount').value;
 		window.open("generated.html");
@@ -66,6 +67,7 @@ $(function() {
 			firstRow.cells[i].setAttribute("style", "background-color: "+getColor);
 		}
 	});
+
 	$("#themeSettings").click(function(){
 		 $('#themeContent').slideToggle('slow');
 	});
@@ -84,7 +86,69 @@ $(function() {
 		getTable.setAttribute("style", "font-family: "+family);
 		
 	});
-	
+	$("#font-color").on('input change', function(){
+		var val = document.getElementById("font-color").value;
+		console.log(val);
+		var getTable = document.getElementById("containTable").childNodes[1];
+		switch(val){
+			case "1":
+				document.getElementById("currentColor").innerHTML = "Juoda";
+				document.getElementById("currentColor").style.color = "black";
+				var getColor = "black";
+				break;
+			case "2":
+				document.getElementById("currentColor").innerHTML = "Geltona";
+				document.getElementById("currentColor").style.color = "yellow";
+				var getColor = "yellow";
+				break;
+			case "3":
+				document.getElementById("currentColor").innerHTML = "Raudona";
+				document.getElementById("currentColor").style.color = "red";
+				var getColor = "red";
+				break;
+			case "4":
+				document.getElementById("currentColor").innerHTML = "Mėlyna";
+				document.getElementById("currentColor").style.color = "blue";
+				var getColor = "blue";
+				break;
+			case "5":
+				document.getElementById("currentColor").innerHTML = "Žalia";
+				document.getElementById("currentColor").style.color = "green";
+				var getColor = "green";
+				break;
+			case "6":
+				document.getElementById("currentColor").innerHTML = "Violetinė";
+				document.getElementById("currentColor").style.color = "purple";
+				var getColor = "purple";
+				break;
+			case "7":
+				document.getElementById("currentColor").innerHTML = "Ruda";
+				document.getElementById("currentColor").style.color = "brown";
+				var getColor = "brown";
+				break;
+			case "8":
+				document.getElementById("currentColor").innerHTML = "Pilka";
+				document.getElementById("currentColor").style.color = "gray";
+				var getColor = "gray";
+				break;
+			case "9":
+				document.getElementById("currentColor").innerHTML = "Tamsiai pilka";
+				document.getElementById("currentColor").style.color = "darkgray";
+				var getColor = "darkgray";
+				break;
+			case "10":
+				document.getElementById("currentColor").innerHTML = "Balta";
+				document.getElementById("currentColor").style.color = "black";
+				var getColor = "white";
+				break;
+		}
+		for(var i = 0; i < getTable.rows.length; i++){
+			for(var j = 0; j < getTable.rows[i].cells.length; j++){
+				getTable.rows[i].cells[j].style.color = getColor;
+			}
+			
+		}
+	});
 });
 console.log(window.location.pathname);
 	if (window.location.pathname == '/F:/Desktop/5uzd/generated.html'){
@@ -99,9 +163,27 @@ function generateTable(){
 		var clmnTitles=  JSON.parse(retrieveArray);
 		var tableHeight = sessionStorage.getItem("height");
 		var tableWidth = sessionStorage.getItem("width");
+		var borderIndex = sessionStorage.getItem("borderStyle");
+
+		if(borderIndex!=null && borderIndex != undefined){
+			switch(borderIndex){
+				case "1":
+					var borderStyle = "border-style: double";
+					break;
+				case "2":
+					var borderStyle = "border-style: solid";
+					break;
+			    case "3":
+			    	var borderStyle = "border-radius: 14px; overflow:hidden; border: 0;";
+			    	break;
+			    case "4":
+			    	var borderStyle = "border-style: dashed; border-spacing:0px; border-collapse: separate";
+			    	break;
+			}
+		}
 
 		var tbl = document.createElement("table");
-		tbl.setAttribute("style", "min-height:"+tableHeight+"px; min-width:"+tableWidth+"px");
+		tbl.setAttribute("style", "min-height:"+tableHeight+"px; min-width:"+tableWidth+"px;"+borderStyle);
 		var caption = document.createElement("caption");
 		caption.appendChild(document.createTextNode(title));
  		var tblBody = document.createElement("tbody");
